abed0086d32c1c1b3f2802000ebea2e9ed5b2f1f
Alex-Abrams <alexander.abrams99@gmail.com>
2019-09-09 16:57:34 -0700

    Added corrected new line functionality to the comment section

--- frontend/components/comments/comment_show.jsx
+++ frontend/components/comments/comment_show.jsx
@@ -4,28 +4,46 @@ import { Route, Link } from 'react-router-dom';
 class CommentShow extends React.Component {
   constructor(props){
     super(props);
+
+    // this.startNextLine = this.startNextLine.bind(this);
   } // c
 
-  startNextLine(comment) {
-    let commentSplit = [];
-    for(i = 0; i < comment.length; i ++) {
-      if (comment.length <= 50) {
-        return comment;
-      } else {
 
-      }
-    }
-  }
 
   render() {
     const { comment, currentUserName } = this.props;
 
+/////////////////////////////
+    const lineStarter = (comment) => {
+      // let length = comment.length;
+      let lines = comment.length / 50;
+      let round = Math.floor(lines);
 
+      let segments = (lines % 1 === 0) ? round : round + 1;
+      let lineArray = [];
+      let copyComment = comment;
+      let position = 0;
+
+      for (let i = 0; i < segments; i++) {
+        let chunk = copyComment.slice(position, position + 50);
+        lineArray.push(chunk);
+        position += chunk.length;
+      };
+
+      return lineArray;
+    }
+
+    const correctedCommentLines = lineStarter(comment.body);
+
+/////////////////////////////
+// <div className="comment-body">{comment.body}</div>
 
     return(
       <div className="comment">
         <span className="comment-name">{comment.username}</span>
-        <div className="comment-body">{comment.body}</div>
+        <ul>
+            {correctedCommentLines.map((line, index) => <li key={index}>{line}</li> )}
+        </ul>
       </div>
     ); //r
   } // render
--- frontend/components/test_components/test_modal.jsx
+++ frontend/components/test_components/test_modal.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
+// line starter stuff
 
+// 'daw-09di-awid-0wiad-0 wodi aw-id -wai d-0a -0widawi d-wai dia-id-awidmawmd'
 
-class TesterComp extends React.Component {
-  constructor(props){
-    super(props);
-  }
+const lineStarter = (comment) => {
+  let length = comment.length;
+  let lines = comment.length / 50;
+  let round = Math.floor(lines);
 
+  let segments = (lines % 1 === 0) ? round : round + 1;
+  let lineArray = [];
+  let copyComment = comment;
+  let position = 0;
 
-  render() {
+  for (let i = 0; i < segments; i++) {
+    let chunk = copyComment.slice(position, position + 50);
+    lineArray.push(chunk);
+    postion += chunk.length;
+  };
 
-    return(
-      <div>
-        Hello
-      </div>
-    )
-  }
+  return lineArray;
+}
 
-};
-
-export default TesterComp;
+// copyComment = copyComment.slice(51, copyComment.length);
