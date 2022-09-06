9001aaf0abf762d9d13511ea46893600dd5c1db3
alex abrams <alexander.abrams99@gmail.com>
2022-09-05 18:22:01 -0700

    Reverted ruby changes.

---
+++ frontend/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {applyMiddleware, combineReducers, createStore, compose} from 'redux';
+import logger from 'redux-logger';
+import rootReducer from './frontend/reducers/root_reducer';
+
+import FrontPage from './frontend/screens/front_page';
+
+let store = null;
+
+function configureStore(initialState = {}) {
+  if (store) {
+    return store;
+  }
+  const middlewares = [thunk, logger];
+
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middlewares))
+  );
+  return store;
+}
+
+let app_store = configureStore();
+
+
+
+const App: () => Node = () => {
+
+  return (
+    <SafeAreaView>
+      <FrontPage />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
